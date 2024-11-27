2f2bc4b3dd8b115a1069a0d578154c0e51d17201
mcrod <paviamcroddixon14@gmail.com>
2024-11-27 12:50:23 +0800

    add appMe

---
+++ MyApp.ts
@@ -0,0 +1,13 @@
+console.log('hello Typescript')
+/*
+function samp1(aw:string,bw:number){
+ return aw+bw;
+}
+
+let aw ="I am Men ";
+let bw=33;
+
+const res1 = samp1(aw,bw);
+
+console.log(res1); */
+
---
+++ src/MyApp.js
@@ -0,0 +1,8 @@
+console.log('hello Typescript');
+function samp1(aw, bw) {
+    return aw + bw;
+}
+var aw = "I am Men ";
+var bw = 33;
+var res1 = samp1(aw, bw);
+console.log(res1);
